bfe9655d18fa46d8ced04088b3ea75ce7f285057
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2025-02-06 18:13:57 +0200

    footer is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ButtonCard from "./components/ButtonCard"
 import FirstCard from "./components/FirstCard"
+import Footer from "./components/Footer"
 
 const App = () => {
   const image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYYZZAps00sZNyCYGeQLcFLztLaGyoMw9rgaPwoiTamGTPiuFXVXTfmHWDo-wZLA4ieqM&usqp=CAU"
@@ -8,7 +9,7 @@ const App = () => {
       <FirstCard img ={image} description="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla." />
       <div>
       <ButtonCard />
-
+       <Footer />
       </div>
     </div>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 
 const Footer = () => {
   return (
-    <div>
-        
+    <div className="flex justify-center bg-[#333333] gap-x-2 text-sm text-white py-10">
+            <a href="#" target="_blank" className="underline underline-ofset-2 hover:decolation-none">Website Templates </a>
+            <p>create with</p>
+            <a href="#" target="_blank" className="underline underline-ofset-2 hover:decolation-none">Website Builder Software.</a>
     </div>
   )
 }
